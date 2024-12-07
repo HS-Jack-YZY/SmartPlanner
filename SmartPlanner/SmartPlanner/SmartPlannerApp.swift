162d539f95f703561f89a8db511da7b202f6c2cb
906581067@qq.com <906581067@qq.com>
2024-12-07 16:46:49 +0800

    Add initial Xcode project

---
+++ SmartPlanner/SmartPlanner/SmartPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPlannerApp.swift
+//  SmartPlanner
+//
+//  Created by 袁哲奕 on 2024/12/7.
+//
+
+import SwiftUI
+
+@main
+struct SmartPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
